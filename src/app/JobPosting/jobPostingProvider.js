dc9a1e8535470bb9563778d54ac4f90325a24ca9
charBS0701 <chaqhdtjr7@gmail.com>
2023-08-07 12:19:27 +0900

    [FIX] #3 - 구직공고리스트 페이지네이션 최대인덱싱 오류 수정

--- src/app/JobPosting/jobPostingProvider.js
+++ src/app/JobPosting/jobPostingProvider.js
@@ -9,13 +9,25 @@ export const retrieveJobPostingList = async function (
   active_status
 ) {
   try {
+    // 1. 총 데이터의 개수를 알아낸다.
+    const totalCountResult = await jobPostingDao.selectJobPostingListCount();
+    const totalCount = totalCountResult.rows[0].total_count;
+
+    // 2. 주어진 `pageSize`에 따라 총 페이지 수를 계산한다.
+    const totalPage = Math.ceil(totalCount / pageSize);
+
+    // 3. 사용자가 요청한 페이지 번호(`page`)가 총 페이지 수보다 큰지 확인한다.
+    if (page > totalPage) {
+      throw new Error("Page out of bounds");
+    }
+
     const offset = (page - 1) * pageSize;
     const result = await jobPostingDao.selectJobPostingList(
       pageSize,
       offset,
       active_status
     );
-    return result;
+    return { totalCount, totalPage, result };
   } catch (error) {
     logger.error("DB 연결 실패");
     throw error;
@@ -24,47 +36,49 @@ export const retrieveJobPostingList = async function (
 
 // 구직공고 데이터 갯수 세는 함수
 export const retrieveJobPostingListCount = async function () {
-    try {
-      const result = await jobPostingDao.selectJobPostingListCount();
-  
-      return parseInt(result.rows[0]["total_count"]);
-    } catch (error) {
-      logger.error("DB 연결 실패");
-      logger.error(error);
-      throw error;
-    }
-  };
+  try {
+    const result = await jobPostingDao.selectJobPostingListCount();
 
-  // 구직공고 상세 조회
-  export const retrieveJobPostingById = async function (jobPostingId) {
-    try {
-      const result = await jobPostingDao.selectJobPostingById(jobPostingId);
-        
-      return result;
-    } catch (error) {
-      logger.error("DB 연결 실패");
-      logger.error(error);
-      throw error;
-    }
-  };
-  
-  // 구직공고 키워드 검색 함수
-  export const searchWithPagination = async function (keyword, page, pageSize) {
-    try {
-      const offset = (page - 1) * pageSize;
-      const result = await jobPostingDao.selectJobPostingByKeyword(
-        keyword,
-        offset,
-        pageSize
-      );
-      const totalCount = await jobPostingDao.selectJobPostingTotalCountByKeyword(keyword);
-      return {
-        totalCount: totalCount,
-        data: result,
-      };
-    } catch (error) {
-      logger.error("DB 연결 실패");
-      logger.error(error);
-      throw error;
-    }
-  };
+    return parseInt(result.rows[0]["total_count"]);
+  } catch (error) {
+    logger.error("DB 연결 실패");
+    logger.error(error);
+    throw error;
+  }
+};
+
+// 구직공고 상세 조회
+export const retrieveJobPostingById = async function (jobPostingId) {
+  try {
+    const result = await jobPostingDao.selectJobPostingById(jobPostingId);
+
+    return result;
+  } catch (error) {
+    logger.error("DB 연결 실패");
+    logger.error(error);
+    throw error;
+  }
+};
+
+// 구직공고 키워드 검색 함수
+export const searchWithPagination = async function (keyword, page, pageSize) {
+  try {
+    const offset = (page - 1) * pageSize;
+    const result = await jobPostingDao.selectJobPostingByKeyword(
+      keyword,
+      offset,
+      pageSize
+    );
+    const totalCount = await jobPostingDao.selectJobPostingTotalCountByKeyword(
+      keyword
+    );
+    return {
+      totalCount: totalCount,
+      data: result,
+    };
+  } catch (error) {
+    logger.error("DB 연결 실패");
+    logger.error(error);
+    throw error;
+  }
+};
